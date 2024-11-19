ebb36768707b83752c332b976e3e4992c2732c3a
ANJUWATHLEY <anchiwathley@gmail.com>
2024-11-19 15:42:28 +0530

    new changes

--- mhs_app/serializers.py
+++ mhs_app/serializers.py
@@ -35,19 +35,23 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__" 
+  
+
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product=ProductSerializer(read_only=True)
+    cart=serializers.PrimaryKeyRelatedField(queryset=Cart.objects.all())
+
     class Meta:
         model = CartItem
-        fields = "__all__"  
+        fields =['id','cart','product','quantity','cart_total']  
 
 class CartSerializer(serializers.ModelSerializer):
-   
-    cart_items = CartItemSerializer(many=True, read_only=True)  
-
-    class Meta:
+   user=UserSerializer(read_only=True)
+   cart_items=CartItemSerializer(many=True,read_only=True)
+   class Meta:
         model = Cart
-        fields = ['id', 'user', 'cart_items']  
+        fields = ['id', 'user', 'cart_items' ]
 
 class OrderSerializer(serializers.ModelSerializer):
     cart = CartSerializer(read_only=True)  # Include Cart d
